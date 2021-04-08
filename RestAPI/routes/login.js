e4a39731662aa02322991eff7a751e428bae530d
Astru43 <45196326+Astru43@users.noreply.github.com>
2021-04-08 11:47:44 +0300

    Added code to convert base64 strings to hex string

--- RestAPI/routes/login.js
+++ RestAPI/routes/login.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const { model } = require('../models/login_model');
 
 router.post('/', (req, res) => {
-    model.login(req.body.hash, req.body.card_num,
+    console.log(req.body.hash); 
+    var hash_buff = Buffer.from(req.body.hash, "base64");
+    var hash = hash_buff.toString("hex");
+    console.log(hash);
+    model.login(hash, req.body.card_num,
         (err, db_result) => {
             if (err) res.json(err);
             else res.json(db_result[0]);
