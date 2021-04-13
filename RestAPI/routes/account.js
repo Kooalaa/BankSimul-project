d54c12968e45096ae521420bce861cd8f8903bab
Astru43 <45196326+Astru43@users.noreply.github.com>
2021-04-13 22:02:47 +0300

    Added back RestAPI

---
+++ RestAPI/routes/account.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const { model } = require('../models/account_model');
+
+router.get('/:account_id', (req, res) => {
+    model.get_info(req.params.account_id,
+        (err, db_result) => {
+            if (err) res.json(err);
+            else res.json(db_result[0]);
+        }
+    );
+});
+
+router.put('/:account_id', (req, res) => {
+    model.edit_saldo(req.params.account_id, req.body.new_saldo,
+        (err, db_result) => {
+            if (err) res.json(err);
+            else res.json(db_result);
+        }
+    );
+});
+
+module.exports = router;
