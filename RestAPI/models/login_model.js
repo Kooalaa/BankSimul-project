bdcb5702f7460b8d6ef4977b14387d9264be15ce
Astru43 <45196326+Astru43@users.noreply.github.com>
2021-04-13 13:58:02 +0300

    Fixed login

--- RestAPI/models/login_model.js
+++ RestAPI/models/login_model.js
@@ -1,10 +1,31 @@
 const {connection: db} = require('../database');
 
 module.exports.model = {
-    login: (hash, card_num, callback) => {
+    login: (card_num, callback) => {
         return db.query(
-            'select Asiakas_id, Tili_id, id from Kortti where Pin=? and Kortinnumero=?',
-            [hash, card_num],
+            'select * from Kortti where Kortinnumero=?',
+            [card_num],
+            callback
+        );
+    },
+    wrong_pin: (card_id, attempts,callback) => {
+        return db.query(
+            'update Kortti set Väärä_pin=? where id=?',
+            [attempts, card_id],
+            callback
+        );
+    },
+    lock_card: (card_id, callback) => {
+        return db.query(
+            'update Kortti set Lukossa=1, Väärä_pin=0 where id=?',
+            [card_id],
+            callback
+        );
+    },
+    success: (card_id, callback) => {
+        return db.query(
+            'update Kortti set Väärä_pin=3 where id=?',
+            [card_id],
             callback
         );
     }
--- RestAPI/routes/login.js
+++ RestAPI/routes/login.js
@@ -1,16 +1,62 @@
+const { json } = require('express');
 const express = require('express');
 const router = express.Router();
 const { model } = require('../models/login_model');
 
 router.post('/', (req, res) => {
-    console.log(req.body.hash); 
+    console.log(req.body.hash);
     var hash_buff = Buffer.from(req.body.hash, "base64");
     var hash = hash_buff.toString("hex");
     console.log(hash);
-    model.login(hash, req.body.card_num,
+    model.login(req.body.card_num,
         (err, db_result) => {
             if (err) res.json(err);
-            else res.json(db_result[0]);
+            else if (db_result[0].Lukossa == 1) {
+                var ret = {
+                    status: -1,
+                    ids: {},
+                    attempts_left: 0
+                };
+
+                res.json(ret);
+            } else {
+                if (db_result[0].Pin == hash) {
+                    var ret = {
+                        status: 1,
+                        ids: {
+                            card_id: db_result[0].id,
+                            customer_id: db_result[0].Asiakas_id,
+                            account_id: db_result[0].Tili_id
+                        },
+                        attempts_left: 3
+                    };
+                    model.success(db_result[0].id, (err, db_result) => {
+                        if (err) res.json(err);
+                        else res.json(ret);
+                    });
+
+                } else {
+                    var attempts = db_result[0].Väärä_pin - 1;
+                    var ret = {
+                        status: 0,
+                        ids: {},
+                        attempts_left: attempts
+                    };
+
+                    if (attempts == 0) model.lock_card(db_result[0].id, (err, db_result) => {
+                        if (err) res.json(err);
+                        else {
+                            ret.status = -1;
+                            res.json(ret);
+                        }
+                    });
+                    else model.wrong_pin(db_result[0].id, attempts, (err, db_result) => {
+                        if (err) res.json(err);
+                        else res.json(ret);
+                    });
+
+                }
+            }
         }
     );
 });
