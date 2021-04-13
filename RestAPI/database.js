2fbd372f1077f307e204176de30cdb754e386805
Kooalaa <konsta.alajarvi@gmail.com>
2021-04-13 21:00:59 +0300

    Deleted RestAPI and DLLPincode

--- RestAPI/database.js
+++
@@ -1,7 +0,0 @@
-const mysql = require('mysql');
-module.exports.connection = mysql.createPool({
-    host: 'localhost',
-    user: 'netuser',
-    password: 'netuser',
-    database: 'BankSimulDB'
-});
