6d0ed6fceca780d9851d61cb9718428574481717
Kooalaa <konsta.alajarvi@gmail.com>
2021-04-16 22:48:08 +0300

    Updated signals and slots on DLLPincode

--- QT_frontend/DLLPincode/dllpincode.h
+++ QT_frontend/DLLPincode/dllpincode.h
@@ -1,23 +1,31 @@
 #ifndef DLLPINCODE_H
 #define DLLPINCODE_H
 
+#include <QObject>
+
 #include "DLLPincode_global.h"
 #include "dialog.h"
+#include "error_dialog.h"
 
-class DLLPINCODE_EXPORT DLLPincode {
+class DLLPINCODE_EXPORT DLLPincode : public QObject {
+    Q_OBJECT
 public:
-    DLLPincode();
+    DLLPincode(QObject *parent = nullptr);
     ~DLLPincode();
     void Main();
-    QByteArray PIN;
+
+signals:
+    void send_pin(QByteArray);
 
 public slots:
     void Wrong_PIN(int attempts);
     void Locked_card();
     void Logged_in();
+    void get_pin(QByteArray);
 
 private:
     Dialog *pdialog;
+    Error_dialog *perror_dialog;
 };
 
 #endif  // DLLPINCODE_H
